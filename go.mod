fd93134592dd7de66494082718d33823f09ad217
Rick Otte <rotte@rmseq.com>
2023-06-20 13:20:11 -0500

    trying something

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rotte55124/Learning-Go-Programming
+
+go 1.20
+
+require github.com/rotte55124/puppy v0.0.0-20230620175737-22f5ac2c54c1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/rotte55124/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	fmt.Println(puppy.Bark())
 }
